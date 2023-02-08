0ca067800cd66f3602e4a646e68cd28b8af4bad1
Devin <devinkearney@gmail.com>
2023-02-07 22:17:14 -0400

    Created db relationships in index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,49 @@
 const User = require('./User');
+const JobPosting = require('./JobPosting');
+const Tag = require('./Tag');
+const Company = require('./Company');
+const SkillTag = require('./SkillTag');
+
+JobPosting.belongsTo(Company, {
+    foreignKey: 'company_id',
+    onDelete: 'CASCADE'
+});
+
+Company.hasMany(JobPosting, {
+    foreignKey: 'company_id',
+    onDelete: 'CASCADE'
+});
+
+Company.belongsToMany(Tag, {
+    through: {
+        model: SkillTag
+    }
+});
+
+Tag.belongsToMany(Company, {
+    through: {
+        model: SkillTag
+    }
+});
+
+User.belongsToMany(Tag, {
+    through: {
+        model: SkillTag
+    }
+});
+
+Tag.belongsToMany(User, {
+    through: {
+        model: SkillTag
+    }
+});
 
 //Export your models so that it can be required in the server.js
 // and/or routes files
-module.exports = { User };
+module.exports = { 
+    User,
+    JobPosting,
+    Tag,
+    Company,
+    SkillTag
+ };
