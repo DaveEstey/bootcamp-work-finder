9f058f53c2ec92a7ff21db8980a3ac5d4fbd41fa
Devin <devinkearney@gmail.com>
2023-02-08 22:13:30 -0400

    Added tag delete, find one by ID and create new routes.

--- controllers/api/tag-routes.js
+++ controllers/api/tag-routes.js
@@ -21,4 +21,59 @@ router.get('/', async (req, res) => {
     }
 });
 
+// get one tag by its `id` value
+router.get('/:id', async (req, res) => {
+    try {
+        const tagData = await Tag.findByPk(req.params.id, {
+            include: [
+                { 
+                    model: User, 
+                    attributes: ['id', 'user_firstName'] 
+                },
+                { 
+                    model: JobPosting, 
+                    attributes: ['id', 'job_title'] 
+                },
+            ],
+        });
+        if (!tagData) {
+            res.status(404).json({ message: 'No tag found with this id!' });
+            return;
+        }
+        res.status(200).json(tagData);
+        return;
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// create a new tag
+router.post('/', async (req, res) => {
+    try {
+        const tagData = await Tag.create(req.body);
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// delete a tag by its `id` value
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const tagData = await Tag.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!tagData) {
+            res.status(404).json({ message: 'No tag found with this id!' });
+            return;
+        }
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+               
 module.exports = router;
