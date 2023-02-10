fbc682f3928bbf54ac40bb6aa439ce5f45d41a30
Dave Estey <daveestey.working@gmail.com>
2023-02-10 02:08:38 -0400

    added some home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,25 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
-  res.render('jobs');
+  res.render('register');
 });
+
+router.get('/login', async (req, res) => {
+  // Send the rendered Handlebars.js template back as the response
+  res.render('login');
+});
+
+router.get('/findjobs', async (req, res) => {
+  // Send the rendered Handlebars.js template back as the response
+  res.render('findjobs');
+});
+
+router.get('/postjob', async (req, res) => {
+  // Send the rendered Handlebars.js template back as the response
+  res.render('postjob');
+});
+
+
+
 
 module.exports = router;
