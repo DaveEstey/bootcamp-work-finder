b8b2cb3b2bf583e3f2dc1667e13ddc6eebeafa6b
Dave Estey <daveestey.working@gmail.com>
2023-02-10 18:05:47 -0400

    routing to other pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,8 +17,22 @@ router.get('/findjobs', async (req, res) => {
 
 router.get('/postjob', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
+  /* if (req.session.loggedIn){
   res.render('postjob');
+  }
+  else {
+    res.redirect('login')
+  } */
+
+  res.render('postjob')
+  
 });
+
+router.get('/jobs', async (req, res) => {
+  
+  
+  res.render('jobs', { jobs })
+})
 
 
 
