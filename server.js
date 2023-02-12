e3fef481cad209f23cc6df8b2e808fa2ca1278a7
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-02-12 18:49:51 -0400

    set up express session secret key

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
+const passport = require('passport');
 //TODO: Uncomment to make use of database, once set up
 const sequelize = require('./config/connection');
+// for env 
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,8 +21,18 @@ app.use(express.static("public"));
 //Look at /controllers folder
 app.use("/", routes);
 
+// express session secret key | For passport
+app.use(session({
+    secret: process.env.secret,
+    resave: true,
+    saveUninitialized: true
+}));
+
+
+
+
 //TODO: Uncomment to make use of database, once set up
-sequelize.sync({force: false}).then(()=>{
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.log(`Server is listening at http://localhost:${PORT}`)
     });
