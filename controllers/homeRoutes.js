4b4f081d9d95290ddd81144d0eec6f45ea7170df
Dave Estey <daveestey.working@gmail.com>
2023-02-12 02:27:50 -0400

    added the handlebars for a couple things, need group input for other parts.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { JobPosting, Tag, Company } = require('../models');
 
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
@@ -12,27 +13,60 @@ router.get('/login', async (req, res) => {
 
 router.get('/findjobs', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
-  res.render('findjobs');
+  try {
+    const jobPostings = await JobPosting.findAll({
+        include: [
+            {
+                model: Tag,
+                model: Company,
+            }],
+        });
+    const plainJobPostings = jobPostings.map((data) => data.get({ plain: true }));
+    console.log(plainJobPostings)
+        res.render('findjobs', { plainJobPostings });
+} catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+} 
 });
 
-router.get('/postjob', async (req, res) => {
+/* router.get('/postjob', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
   /* if (req.session.loggedIn){
   res.render('postjob');
   }
   else {
     res.redirect('login')
-  } */
+  } 
 
   res.render('postjob')
-  
+
+});
+ */
+ router.get('/jobs', async (req, res) => {
+    try {
+        const jobPostings = await JobPosting.findAll({
+            include: [
+                {
+                    model: Tag,
+                    model: Company,
+                }],
+            });
+        const plainJobPostings = jobPostings.map((data) => data.get({ plain: true }));
+        console.log(plainJobPostings)
+            res.render('jobs', { plainJobPostings });
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err);
+    } 
 });
 
-router.get('/jobs', async (req, res) => {
-  
-  
-  res.render('jobs', { jobs })
-})
+
+
+router.get('/contactus', async (req, res) => {
+  // Send the rendered Handlebars.js template back as the response
+  res.render('contactus');
+});
 
 
 
