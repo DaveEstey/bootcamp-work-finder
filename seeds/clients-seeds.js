7d6e68a77a87892fe843fd9c9725290f359e9e53
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-02-15 11:01:24 -0400

    nth changes

--- seeds/clients-seeds.js
+++ seeds/clients-seeds.js
@@ -3,46 +3,46 @@ const { User } = require('../models');
 const userData = [
 
     {
-    user_email: "cfanthome0@infoseek.co.jp",
-    user_password: "kPf3IHJ58iN",
-    user_firstName: "Charmine",
-    user_lastName: "Fanthome"
-    }, 
+        user_email: "cfanthome0@infoseek.co.jp",
+        user_password: "kPf3IHJ58iN",
+        user_firstName: "Charmine",
+        user_lastName: "Fanthome"
+    },
     {
-    user_email: "kscudder1@cafepress.com",
-    user_password: "LJ5IvQLWKoJe",
-    user_firstName: "Karole",
-    user_lastName: "Scudder"
-    }, 
+        user_email: "kscudder1@cafepress.com",
+        user_password: "LJ5IvQLWKoJe",
+        user_firstName: "Karole",
+        user_lastName: "Scudder"
+    },
     {
-    user_email: "mlindborg2@npr.org",
-    user_password: "ClgPxr2oG",
-    user_firstName: "Meyer",
-    user_lastName: "Lindborg"
-    }, 
+        user_email: "mlindborg2@npr.org",
+        user_password: "ClgPxr2oG",
+        user_firstName: "Meyer",
+        user_lastName: "Lindborg"
+    },
     {
-    user_email: "hshirley3@harvard.edu",
-    user_password: "97DalrCW",
-    user_firstName: "Hermon",
-    user_lastName: "Shirley"
-    }, 
+        user_email: "hshirley3@harvard.edu",
+        user_password: "97DalrCW",
+        user_firstName: "Hermon",
+        user_lastName: "Shirley"
+    },
     {
-    user_email: "sscinelli4@instagram.com",
-    user_password: "asaOe2Z",
-    user_firstName: "Skippy",
-    user_lastName: "Scinelli"
-    }, 
+        user_email: "sscinelli4@instagram.com",
+        user_password: "asaOe2Z",
+        user_firstName: "Skippy",
+        user_lastName: "Scinelli"
+    },
     {
-    user_email: "btrathan5@businesswire.com",
-    user_password: "AbbSz7plXIB",
-    user_firstName: "Barrie",
-    user_lastName: "Trathan"
-    }, 
+        user_email: "btrathan5@businesswire.com",
+        user_password: "AbbSz7plXIB",
+        user_firstName: "Barrie",
+        user_lastName: "Trathan"
+    },
     {
-    user_email: "egreen6@wired.com",
-    user_password: "0JADhBu2QB",
-    user_firstName: "Emerson",
-    user_lastName: "Green"
+        user_email: "egreen6@wired.com",
+        user_password: "0JADhBu2QB",
+        user_firstName: "Emerson",
+        user_lastName: "Green"
     }
 ];
 
