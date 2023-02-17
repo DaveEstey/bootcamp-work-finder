dabd62c414abf5bc1efb9b14ecff689eb2144d0e
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-02-16 20:54:04 -0400

    added connect-flash package & initialize

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,15 +6,12 @@ const { Op } = require("sequelize");
 const session = require('express-session');
 
 
+
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
   res.render('register');
 });
 
-router.get('/login', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render('login');
-});
 
 // router.post('/login', passport.authenticate('local',),
 //   function (req, res) {
@@ -47,7 +44,8 @@ router.get('/findjobs', withAuth, async (req, res) => {
     });
     const plainJobPostings = jobPostings.map((data) => data.get({ plain: true }));
     console.log(plainJobPostings)
-    res.render('findjobs', { plainJobPostings });
+
+    res.render('findjobs', { plainJobPostings, logged_in: true });
   } catch (err) {
     res.status(500).json(err);
     console.log(err);
@@ -67,10 +65,13 @@ router.get('/findjobs', withAuth, async (req, res) => {
  
 });
  */
+
+// const errorMessage = 'Incorrect email or password.';
 router.get('/login', async (req, res) => {
+
   if (req.session.loggedIn) {
     res.redirect('/findjobs');
-    return;   
+    return;
   }
   res.render('login');
 });
@@ -80,11 +81,13 @@ router.post('/login', async (req, res) => {
     const userData = await User.findOne({ where: { user_email: req.body.email } });
     if (!userData) {
       res.status(400).json({ message: 'No user with that email address!' });
+      // req.flash('error', 'Something Wrong');
       return;
     }
     const validPassword = await userData.checkPassword(req.body.password);
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect password!' });
+      // req.flash('error', 'Something Wrong');
       return;
     }
 
@@ -100,7 +103,7 @@ router.post('/login', async (req, res) => {
 });
 
 
-router.get('/jobs', async (req, res) => {
+router.get('/jobs', withAuth, async (req, res) => {
   try {
     const jobPostings = await JobPosting.findAll({
       include: [
@@ -111,7 +114,7 @@ router.get('/jobs', async (req, res) => {
     });
     const plainJobPostings = jobPostings.map((data) => data.get({ plain: true }));
     console.log(plainJobPostings)
-    res.render('jobs', { plainJobPostings });
+    res.render('jobs', { plainJobPostings, logged_in: true });
   } catch (err) {
     res.status(500).json(err);
     console.log(err);
@@ -120,7 +123,14 @@ router.get('/jobs', async (req, res) => {
 
 router.get('/contactus', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
-  res.render('contactus');
+  res.render('contactus', { messages: req.flash() });
+});
+
+router.post('/contactus', async (req, res) => {
+  // Send the rendered Handlebars.js template back as the response
+
+  req.flash('success', 'Thank you for your message!');
+  res.redirect('/contactus');
 });
 
 
@@ -132,60 +142,60 @@ router.get('/skills', withAuth, async (req, res) => {
         {
           model: Tag
         }],
-      });
-      const excludedTags = userData.tags.map((excluded) => excluded.get(userData.tags.id));
-      console.log(excludedTags);
-      const excludedArr = [];
-      excludedTags.forEach(element => {
-        excludedArr.push(element.id);
-      });
-      const tagData = await Tag.findAll({
-        where: {
-          id: {
-            [Op.not]: excludedArr
-          }
+    });
+    const excludedTags = userData.tags.map((excluded) => excluded.get(userData.tags.id));
+    console.log(excludedTags);
+    const excludedArr = [];
+    excludedTags.forEach(element => {
+      excludedArr.push(element.id);
+    });
+    const tagData = await Tag.findAll({
+      where: {
+        id: {
+          [Op.not]: excludedArr
         }
-      });
-    res.status(200).json({userData, tagData});
+      }
+    });
+    res.status(200).json({ userData, tagData });
   } catch (err) {
     res.status(500).json(err);
     console.log(err);
   }
 });
 
-  //Update a user's skills, used for the skills view.
-  router.put('/skills', async (req, res) => {
-    User.update(req.body, {
-      where: {
-          id: req.session.user_id,
-      },
+//Update a user's skills, used for the skills view.
+router.put('/skills', async (req, res) => {
+  User.update(req.body, {
+    where: {
+      id: req.session.user_id,
+    },
   })
-      .then((user) => {
-          return SkillTag.findAll({ where: { user_id: req.session.user_id } });
-      })
-      .then((skillTags) => {
-          const skillTagIds = skillTags.map(({ tag_id }) => tag_id);
-          const newSkillTags = req.body.tagIds
-              .filter((tag_id) => !skillTagIds.includes(tag_id))
-              .map((tag_id) => {
-                  return {
-                      user_id: req.session.user_id,
-                      tag_id,
-                  };
-              });
-          const skillTagsToRemove = skillTags
-              .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-              .map(({ id }) => id);
+    .then((user) => {
+      return SkillTag.findAll({ where: { user_id: req.session.user_id } });
+    })
+    .then((skillTags) => {
+      const skillTagIds = skillTags.map(({ tag_id }) => tag_id);
+      const newSkillTags = req.body.tagIds
+        .filter((tag_id) => !skillTagIds.includes(tag_id))
+        .map((tag_id) => {
+          return {
+            user_id: req.session.user_id,
+            tag_id,
+          };
+        });
+      const skillTagsToRemove = skillTags
+        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .map(({ id }) => id);
 
-          return Promise.all([
-              SkillTag.destroy({ where: { id: skillTagsToRemove } }),
-              SkillTag.bulkCreate(newSkillTags),
-          ]);
-      })
-      .then((updatedSkillTags) => res.json(updatedSkillTags))
-      .catch((err) => {
-          res.status(400).json(err);
-      });
+      return Promise.all([
+        SkillTag.destroy({ where: { id: skillTagsToRemove } }),
+        SkillTag.bulkCreate(newSkillTags),
+      ]);
+    })
+    .then((updatedSkillTags) => res.json(updatedSkillTags))
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 //   User.update(req.body, {
 //     individualHooks: true,
@@ -224,5 +234,15 @@ router.get('/skills', withAuth, async (req, res) => {
 // });
 
 
+// --------------Logout-----------------------//
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
