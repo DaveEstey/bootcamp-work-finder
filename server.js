8ddfc2182cd4d386a621773dbc13e01c709b9cf2
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-02-16 20:58:01 -0400

    nothing changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 // const passport = require('passport');
-// const flash = require('flash')
+const flash = require('connect-flash');
 //TODO: Uncomment to make use of database, once set up
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -31,31 +31,34 @@ app.use(express.static("public"));
 // }));
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-  app.use(session(sess));
+app.use(session(sess));
 // initialize passport
 // app.use(passport.session());
 // app.use(passport.initialize());
 
 // initialize flash
-// app.use(flash());
+app.use(flash());
+
+
+
 
 app.use("/", routes);
 
 
 //TODO: Uncomment to make use of database, once set up
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is listening at http://localhost:${PORT}`)
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`)
+  });
 });
 
 // app.listen(PORT, () => {
